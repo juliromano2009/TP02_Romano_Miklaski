b4cfd09c70b80e3e3a83ed21b4d023a541cdfc04
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-05-13 09:00:04 -0300

    sugeridorReceta arreglado

--- Models/SugeridorReceta.cs
+++ Models/SugeridorReceta.cs
@@ -2,53 +2,57 @@ namespace TP02.Models;
 
 public class SugeridorRecetas
 {
-    public string nombreCocinero {get; set;};
-    public date fechaNacimiento {private get; set;};
-    public string TipoComida {private get; set;};
-    public double presupuestoDisponible {private get; set;};
-    public int cantComensales {private get; set;};
+    public string nombreCocinero {get; set;}
+    public DateTime fechaNacimiento {private get; set;}
+    public string TipoComida {private get; set;}
+    public double presupuestoDisponible {private get; set;}
+    public int cantComensales {private get; set;}
 
     public int calcularEdad()
     {
-        int edad;
-        int hoy = DateTime.Today;
+        int edad = DateTime.Today.Year - fechaNacimiento.Year;
 
-        edad = hoy - fechaNacimiento;
+        if(DateTime.Today.Month < fechaNacimiento.Month && DateTime.Today.Day < fechaNacimiento.Day)
+        {
+            edad--;
+        }
 
         return edad;
     }
 
     public string determinarPlato()
     {
+        string devolucion = null;
         if(TipoComida == "caliente")
         {
-            if(presupuesto  < 3000)
+            if(presupuestoDisponible  < 3000)
             {
-                return "Fideos con manteca";
+                devolucion = "Fideos con manteca";
             }
-            else if(presupuesto >= 3000 && presupuesto <= 7000)
+            else if(presupuestoDisponible >= 3000 && presupuestoDisponible <= 7000)
             {
-                return "Arroz con verduras salteadas";
+                devolucion = "Arroz con verduras salteadas";
             }
-            else if(presupuesto > 7000)
+            else if(presupuestoDisponible > 7000)
             {
-                return "Pollo al horno con guarnición";
+                devolucion = "Pollo al horno con guarnición";
             }
         }
         else if(TipoComida == "fria")
         {
-            if(presupuesto  < 3000)
+            if(presupuestoDisponible  < 3000)
             {
-                return "Ensalada simple";
+                devolucion = "Ensalada simple";
             }
-            else if(presupuesto >= 3000 && presupuesto <= 7000)
+            else if(presupuestoDisponible >= 3000 && presupuestoDisponible <= 7000)
             {
-                return "Ensalada completa con proteína";
+                devolucion = "Ensalada completa con proteína";
             }
-            else if(presupuesto > 7000)
+            else if(presupuestoDisponible > 7000)
             {
-                return "Tabla de fiambres y quesos";
+                devolucion = "Tabla de fiambres y quesos";
             }
         }
+        return devolucion;
     }
 }
